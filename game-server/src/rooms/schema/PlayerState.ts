56fcff4612b4a57670eda40a9a395189e0483b13
darknil developer <jrmlelantus@gmail.com>
2025-01-30 17:40:14 +0200

    fix map schema transfering player

--- game-server/src/rooms/schema/PlayerState.ts
+++ game-server/src/rooms/schema/PlayerState.ts
@@ -3,7 +3,6 @@ import { Card } from './Card'
 export class PlayerState extends Schema {
   @type('string') id: string = ''
   @type('string') name: string = ''
-  @type('number') seatIndex: number | null = null
   @type('number') chips = 100
   @type([Card]) openCards = new Array<Card>()
   @type('number') currentBet = 0
